5b4fb47e89dc716ff01235cc83a54c4792c5c41a
byxor <brandnewbyxor@gmail.com>
2016-05-12 19:47:46 +0100

    Add Rect class to shapes

--- src/shape.py
+++ src/shape.py
@@ -7,7 +7,17 @@ class Shape(object):
 		pass
 
 
-class Square(Shape):
+class Rect(Shape):
 
-	def draw(self, mImage, position, size):
-		pass
+	def draw(self, mImage, pos, size, colour):
+		"""Draws a rectangle on the provided MagentaImage, at the specified
+		raw_position, with the specified size, with the specified colour. The
+		drawn shape is upscaled based on the MagentaImage's scale attribute."""
+
+		xpos = pos[0]
+		ypos = pos[1]
+		width = size[0]
+		height = size[1]
+		for x in range(xpos, xpos+width):
+			for y in range(ypos, ypos+width):
+				mImage.put_scaledpixel((x, y), colour)
