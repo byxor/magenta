6e9d8a2d4f3380a27e9c254467e5098956576cd2
byxor <brandnewbyxor@gmail.com>
2016-05-12 21:26:01 +0100

    Make unit tests compatible with new tuple arguments

--- src/magenta_test.py
+++ src/magenta_test.py
@@ -19,45 +19,45 @@ class MagentaTest(unittest.TestCase):
 		self.assertGreaterEqual(colour[2], 0)
 
 	def test_MagentaImage_string(self):
-		mImage = MagentaImage(1, 1, 2)
+		mImage = MagentaImage((1, 1), 2)
 		expected = "---MagentaImage---\nRaw dimensions: (1x1)\nScaled dimensions: (2x2) scale=2"
 		self.assertEqual(str(mImage), expected)
-		mImage = MagentaImage(10, 5, 1)
+		mImage = MagentaImage((10, 5), 1)
 		expected = "---MagentaImage---\nRaw dimensions: (10x5)\nScaled dimensions: (10x5) scale=1"
 		self.assertEqual(str(mImage), expected)
 
 	def test_MagentaImage_argvalidation(self):
 		failed = False
 		try:
-			mImage = MagentaImage(20, 20, "bug")
+			mImage = MagentaImage((20, 20), "bug")
 		except ValueError:
 			failed = True
 		self.assertEqual(failed, True)
 
 		failed = False
 		try:
-			mImage = MagentaImage(20, 20, "bug")
+			mImage = MagentaImage((20, 20), "bug")
 		except ValueError:
 			failed = True
 		self.assertEqual(failed, True)
 
 		failed = False
 		try:
-			mImage = MagentaImage(20, 20, 5)
+			mImage = MagentaImage((20, 20), 5)
 		except ValueError:
 			failed = True
 		self.assertEqual(failed, False)
 
 	def test_MagentaImage_rawsize(self):
-		mImage = MagentaImage(20, 20, 4)
+		mImage = MagentaImage((20, 20), 4)
 		self.assertEqual(mImage.get_rawsize(), (20, 20))
 
 	def test_MagentaImage_scaledsize(self):
-		mImage = MagentaImage(20, 10, 5)
+		mImage = MagentaImage((20, 10), 5)
 		self.assertEqual(mImage.get_scaledsize(), (100, 50))
 
 	def test_MagentaImage_save(self):
-		mImage = MagentaImage(1, 1, 1)
+		mImage = MagentaImage((1, 1), 1)
 		fileexists = True
 		try:
 			mImage.save("temp.png", "png")
