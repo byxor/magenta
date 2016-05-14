5df9ca978851c559be7cc4e3ac46c8d94343e89c
byxor <brandnewbyxor@gmail.com>
2016-05-14 01:15:08 +0100

    Add line drawing method

--- magenta/mag_graphics.py
+++ magenta/mag_graphics.py
@@ -1,7 +1,8 @@
 from queue import Queue
 
+
 def floodfill(mImage, root, fill):
-	"""Iteratively floodfill a MImage."""
+	"""Iteratively floodfill a MagentaImage."""
 
 	old_colour = mImage.get_rgb(root)
 	q = Queue()
@@ -18,3 +19,20 @@ def floodfill(mImage, root, fill):
 		q.enqueue((pos[0]+1, pos[1]+0))	# Enqueue the eastern cell
 		q.enqueue((pos[0]+0, pos[1]+1))	# Enqueue the southern cell
 		q.enqueue((pos[0]-1, pos[1]+0))	# Enqueue the wester cell
+
+
+def line(mImage, start, end, colour):
+	"""Draw a line from A to B on a MagentaImage."""
+
+	x1 = start[0]
+	y1 = start[1]
+
+	x2 = start[0]
+	y2 = start[1]
+
+	dy = y2 - y1
+	dx = x2 - x1
+
+	for x in range(x1, x2):
+		y = y1 + dy * (x - x1) / dx
+		mImage.put_rgb((x,y), colour)
